e417178bff8763e9cb5ecfd8e289801be9ca28e9
Rakibul Hassan Rony <rakibulhassan.rony123@gmail.com>
2023-03-25 16:45:42 +0600

    song list doen

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: '/V-Music-Firebase/'
+  // public: '/V-Music-Firebase/'
 })
